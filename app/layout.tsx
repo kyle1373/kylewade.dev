85ad95132e00c2493b73ded88a58b0e396418d3e
synthful <59634395+kyle1373@users.noreply.github.com>
2024-09-02 10:32:39 -0700

    Added stats logging

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -6,7 +7,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kyle's Website",
-  description: "This is my website, which contains all of my past projects, experiences, and more.",
+  description:
+    "This is my website, which contains all of my past projects, experiences, and more.",
 };
 
 export default function RootLayout({
@@ -16,6 +18,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      {process.env.NODE_ENV === "production" && (
+        <Script
+          async
+          src="https://stats.superfx.dev/script.js"
+          data-website-id="09dca433-66b6-4799-a145-5b4d2e20d307"
+        />
+      )}
       <body className={inter.className}>{children}</body>
     </html>
   );
